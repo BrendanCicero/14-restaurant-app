195c1da8f7bd5de635a6c446eeb84019d5f64cff
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 16:23:04 +0000

    Update app.js

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -24,10 +24,10 @@ class App {
     const page = routes[url];
     this._content.innerHTML = await page.render();
     await page.afterRender();
-    const skipLinkElem = document.querySelector(".skip-anchor");
+    const skipLinkElem = document.querySelector(".skip-link");
     skipLinkElem.addEventListener("click", (event) => {
       event.preventDefault();
-      document.querySelector("#main-content").focus();
+      document.querySelector("#maincontent").focus();
     });
   }
 }
