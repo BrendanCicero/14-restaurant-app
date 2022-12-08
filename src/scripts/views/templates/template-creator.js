b03717481d0ef69a60c2955086129e5860357f71
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 16:23:13 +0000

    Update template-creator.js

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,24 +1,26 @@
 import CONFIG from "../../globals/config";
 
 const createRestaDetailTemplate = (resta) => `
-  <h2 class="resta__title">${resta.title}</h2>
-  <img class="resta__poster" src="${
-    CONFIG.BASE_IMAGE_URL + resta.poster_path
-  }" alt="${resta.title}" />
-  <div class="resta__info">
-    <h3>Information</h3>
-    <h4>Tagline</h4>
-    <p>${resta.tagline}</p>
-    <h4>Release Date</h4>
-    <p>${resta.release_date}</p>
-    <h4>Duration</h4>
-    <p>${resta.runtime} minutes</p>
-    <h4>Rating</h4>
-    <p>${resta.vote_average}</p>
-  </div>
-  <div class="resta__overview">
-    <h3>Overview</h3>
-    <p>${resta.overview}</p>
+  <div class='wrapper'>
+    <div class='center'>
+      <h2 class="resta__header">${resta.name}</h2>
+      <img class="resta__poster" src="${
+        CONFIG.BASE_IMAGE_URL + resta.pictureId
+      }" alt="${resta.name}" />
+      <h3>Information</h3>
+    </div>
+    <div class="resta__info">
+      <h4>Alamat</h4>
+      <p>${resta.address}</p>
+      <h4>Kota</h4>
+      <p>${resta.city}</p>
+      <h4>Rating</h4>
+      <p>${resta.rating}</p>
+    </div>
+    <div class="resta__overview">
+      <h3>Deskripsi</h3>
+      <p>${resta.description}</p>
+    </div>
   </div>
 `;
 
@@ -30,7 +32,9 @@ const createRestaItemTemplate = (resta) => `
         <p tabindex='0' class='resta__city'>${resta.city}</p>
         <div class='resta__content'>
             <p tabindex='0' class='resta__rating'>Rating : ${resta.rating}</p>
-            <h2 tabindex='0' class='resta__title'>${resta.name}</h2>
+            <h2 tabindex='0' class='resta__title'><a href="/#/detail/${
+              resta.id
+            }">${resta.name}</a></h2>
             <p tabindex='0' class='resta__description'>${resta.description} </p>
         </div>
     </article>
