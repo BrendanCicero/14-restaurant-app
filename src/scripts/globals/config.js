c2b9f6531e8a348e53ed7666a33bb4e1798a8d94
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 23:03:56 +0000

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -4,6 +4,9 @@ const CONFIG = {
   BASE_IMAGE_URL: "https://restaurant-api.dicoding.dev/images/medium/",
   DEFAULT_LANGUAGE: "en-us",
   CACHE_NAME: 'OurResta-V1',
+  DATABASE_NAME: 'our-resta-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurant',
 };
 
 export default CONFIG;
