a089f148e7f4664201f7d31d61ec5b831764951b
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 11:53:11 +0000

    Update home.js

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,3 +1,5 @@
+import RestaurantSource from "../../data/restaurant-source";
+
 const Home = {
   async render() {
     return `
@@ -6,7 +8,8 @@ const Home = {
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const cafe = await RestaurantSource.getRestaurantList();
+    console.log(cafe);
   },
 };
 
