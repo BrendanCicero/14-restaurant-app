9b873549d34aaaaa3d4416e110cf0cf6ee4a6baa
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 16:23:11 +0000

    Update detail.js

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,5 +1,6 @@
 import UrlParser from "../../routes/url-parser";
 import RestaurantSource from "../../data/restaurant-source";
+import { createRestaDetailTemplate } from "../templates/template-creator";
 
 const Detail = {
   async render() {
@@ -11,7 +12,10 @@ const Detail = {
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const resta = await RestaurantSource.getRestaurantDetail(url.id);
-    console.log(resta);
+    const restaItem = resta.restaurant;
+    console.log(restaItem)
+    const restaDetail = document.querySelector("#resta__detail");
+    restaDetail.innerHTML = createRestaDetailTemplate(restaItem);
   },
 };
 
