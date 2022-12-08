77c8443f340674300fb4b878b39f1bb190809099
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 07:46:36 +0000

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,28 +3,35 @@ import "../styles/main.css";
 import "../styles/responsive.css";
 import "../styles/skip-link.css";
 import restaurant from "../DATA.json";
+import App from "./views/app";
 
-const menu = document.querySelector("#menu");
+// const menu = document.querySelector("#menu");
 const hero = document.querySelector(".hero");
-const main = document.querySelector("main");
-const drawer = document.querySelector("#drawer");
+// const main = document.querySelector("main");
+// const drawer = document.querySelector("#drawer");
 
-menu.addEventListener("click", function (event) {
-  drawer.classList.toggle("open");
-  event.stopPropagation();
+const app = new App({
+  button: document.querySelector("#menu"),
+  drawer: document.querySelector("#drawer"),
+  content: document.querySelector("#mainContent"),
 });
 
-hero.addEventListener("click", function () {
-  drawer.classList.remove("open");
-});
+// menu.addEventListener("click", (event) => {
+//   drawer.classList.toggle("open");
+//   event.stopPropagation();
+// });
 
-main.addEventListener("click", function () {
-  drawer.classList.remove("open");
-});
+// hero.addEventListener("click", () => {
+//   drawer.classList.remove("open");
+// });
+
+// main.addEventListener("click", () => {
+//   drawer.classList.remove("open");
+// });
 
 const mains = () => {
   let restaList = "";
-  let restaurants = restaurant.restaurants;
+  const { restaurants } = restaurant;
 
   restaurants.forEach((item) => {
     restaList += `
