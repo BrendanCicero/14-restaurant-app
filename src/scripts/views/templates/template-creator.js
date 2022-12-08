6165b9d91fa70624917ba47ead88ef4799eaa7fe
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 23:03:44 +0000

    Update template-creator.js

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -40,4 +40,21 @@ const createRestaItemTemplate = (resta) => `
     </article>
 `;
 
-export { createRestaItemTemplate, createRestaDetailTemplate };
+const createLikeButtonTemplate = () => `
+  <button aria-label="like this movie" id="likeButton" class="like">
+     <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
+`;
+
+const createLikedButtonTemplate = () => `
+  <button aria-label="unlike this movie" id="likeButton" class="like">
+    <i class="fa fa-heart" aria-hidden="true"></i>
+  </button>
+`;
+
+export {
+  createRestaItemTemplate,
+  createRestaDetailTemplate,
+  createLikeButtonTemplate,
+  createLikedButtonTemplate,
+};
