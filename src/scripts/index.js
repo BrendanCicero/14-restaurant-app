6f008b50c88386990127956c328223b112d768e5
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 11:31:57 +0000

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,26 @@
-import "regenerator-runtime"; /* for async await transpile */
+// import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
 import "../styles/responsive.css";
 import "../styles/skip-link.css";
-import restaurant from "../DATA.json";
 import App from "./views/app";
 
 // const menu = document.querySelector("#menu");
-const hero = document.querySelector(".hero");
+// const hero = document.querySelector(".hero");
 // const main = document.querySelector("main");
 // const drawer = document.querySelector("#drawer");
 
 const app = new App({
   button: document.querySelector("#menu"),
   drawer: document.querySelector("#drawer"),
-  content: document.querySelector("#mainContent"),
+  content: document.querySelector("#maincontent"),
+});
+
+window.addEventListener("hashchange", () => {
+  app.renderPage();
+});
+
+window.addEventListener("load", () => {
+  app.renderPage();
 });
 
 // menu.addEventListener("click", (event) => {
@@ -29,25 +36,25 @@ const app = new App({
 //   drawer.classList.remove("open");
 // });
 
-const mains = () => {
-  let restaList = "";
-  const { restaurants } = restaurant;
+// const mains = () => {
+//   let restaList = "";
+//   const { restaurants } = restaurant;
 
-  restaurants.forEach((item) => {
-    restaList += `
-        <article tabindex='0' class='resta__list'>
-            <img tabindex='0' class='resta__thumbnail' src='${item.pictureId}' alt='${item.name}'/>
-            <p tabindex='0' class='resta__city'>${item.city}</p>
-            <div class='resta__content'> 
-              <p tabindex='0' class='resta__rating'>Rating : ${item.rating}</p>
-              <h2 tabindex='0' class='resta__title'>${item.name}</h2>
-              <p tabindex='0' class='resta__description'>${item.description} </p>
-            </div>
-        </article>
-      `;
-  });
+//   restaurants.forEach((item) => {
+//     restaList += `
+//         <article tabindex='0' class='resta__list'>
+//           <img tabindex='0' class='resta__thumbnail' src='${item.pictureId}' alt='${item.name}'/>
+//             <p tabindex='0' class='resta__city'>${item.city}</p>
+//             <div class='resta__content'>
+//               <p tabindex='0' class='resta__rating'>Rating : ${item.rating}</p>
+//               <h2 tabindex='0' class='resta__title'>${item.name}</h2>
+//               <p tabindex='0' class='resta__description'>${item.description} </p>
+//             </div>
+//         </article>
+//       `;
+//   });
 
-  document.getElementById("resta").innerHTML = restaList;
-};
+//   document.getElementById("resta").innerHTML = restaList;
+// };
 
-document.addEventListener("DOMContentLoaded", mains);
+// document.addEventListener("DOMContentLoaded", mains);
