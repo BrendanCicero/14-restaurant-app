0ff94ffd2bb37e480b73d850265e528f7370c802
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 11:38:22 +0000

    Update config.js

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: "restaurant-api",
+  BASE_URL: "https://restaurant-api.dicoding.dev/",
+  BASE_IMAGE_URL: "https://restaurant-api.dicoding.dev/images/medium/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
