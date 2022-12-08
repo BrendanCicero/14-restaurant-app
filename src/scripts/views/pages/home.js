4e839101a6d1c3512553bf5f613e7988f2c896cf
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-08 12:27:40 +0000

    Update home.js

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,15 +1,26 @@
 import RestaurantSource from "../../data/restaurant-source";
+import { createRestaItemTemplate } from "../templates/template-creator";
 
 const Home = {
   async render() {
     return `
-      <h2>Home Page</h2>
+      <section class='content'>
+        <div class='explore'>
+          <h2 class='resta__header'>Explore Restaurant</h2>
+          <div class='resta' id='resta'>
+          
+          </div>
+        </div>
+      </section>
     `;
   },
 
   async afterRender() {
-    const cafe = await RestaurantSource.getRestaurantList();
-    console.log(cafe);
+    const restas = await RestaurantSource.getRestaurantList();
+    const restaList = document.querySelector("#resta");
+    restas.restaurants.forEach((resta) => {
+      restaList.innerHTML += createRestaItemTemplate(resta);
+    });
   },
 };
 
