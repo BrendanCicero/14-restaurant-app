75928b24a90342bb6acc3f746dda14e220582504
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-09 10:16:47 +0000

    Update template-creator.js

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -20,17 +20,59 @@ const createRestaDetailTemplate = (resta) => `
       <p>${resta.description}</p>
     </div>
   </div>
+  
+  <div class="resta-menu">
+    <h3 class='center'>Menu</h3>
+    <div class='resta-menu-grid'>
+      <div class="resta-food">
+        <h4 class='center'>Food</h4>
+        <ul>
+          ${resta.menus.foods.map((food, i) => `
+            <li><p>${i + 1}) ${food.name}</p></li>
+          `).join('')}
+        <ul>
+      </div>
+
+      <div class="resta-drink">
+        <h4 class='center'>Drink</h4>
+        <ul>
+          ${resta.menus.drinks.map((drink, i) => `
+                <li><p>${i + 1}) ${drink.name}</p></li>
+          `).join('')}
+        <ul>
+      </div>
+  </div>
+  </div>
+
+  <div class="review">
+  <h3 class='center'>Review</h3>
+  <p>${resta.customerReviews.map((review) => `
+    <div class="resta-review-item">
+      <div class="review-header">
+        <p class="review-name">${review.name}</p>
+
+        <p class="review-date">${review.date}</p>
+      </div>
+
+      <div class="review-body">
+        ${review.review}
+      </div>
+    </div>
+    `).join('')}</p>
+  </div>
 `;
 
 const createRestaItemTemplate = (resta) => `
   <article tabindex='0' class='resta__list'>
-        <img tabindex='0' class='resta__thumbnail' src='${CONFIG.BASE_IMAGE_URL + resta.pictureId}' alt='${resta.name}'/>
-        <p tabindex='0' class='resta__city'>${resta.city}</p>
-        <div class='resta__content'>
-            <p tabindex='0' class='resta__rating'>Rating : ${resta.rating}</p>
-            <h2 tabindex='0' class='resta__title'><a href="/#/detail/${resta.id}">${resta.name}</a></h2>
-            <p tabindex='0' class='resta__description'>${resta.description} </p>
-        </div>
+        <a href="/#/detail/${resta.id}">
+          <img tabindex='0' class='resta__thumbnail' src='${CONFIG.BASE_IMAGE_URL + resta.pictureId}' alt='${resta.name}'/>
+          <p tabindex='0' class='resta__city'>${resta.city}</p>
+          <div class='resta__content'>
+              <p tabindex='0' class='resta__rating'>Rating : ${resta.rating}</p>
+              <h2 class='resta__title' tabindex='0' >${resta.name}</h2>
+              <p tabindex='0' class='resta__description'>${resta.description} </p>
+          </div>
+        </a>
     </article>
 `;
 
