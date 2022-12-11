33064714e55180afefa075de89f9cd6ade86df25
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-11 11:43:18 +0000

    Update Liking_Resta.spec.js

--- e2e/Liking_Resta.spec.js
+++ e2e/Liking_Resta.spec.js
@@ -7,5 +7,20 @@ Before(({ I }) => {
 
 Scenario("showing empty liked restas", ({ I }) => {
   I.seeElement(".resta");
-  I.see("Tidak ada film untuk ditampilkan", ".resta");
+  I.see("Silahkan tambah resta favoritemu dulu di halaman utama", ".resta");
+});
+
+Scenario("liking one resta", ({ I }) => {
+  I.see("Silahkan tambah resta favoritemu dulu di halaman utama", ".resta");
+
+  I.amOnPage("/");
+
+  I.seeElement(".resta__list a");
+  I.click(locate(".resta__list a").first());
+
+  I.seeElement("#likeButton");
+  I.click("#likeButton");
+
+  I.amOnPage("/#/favorite");
+  I.seeElement(".resta__list");
 });
