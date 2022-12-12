60441cf7593427bb521876037b9cc64b39e01a0b
Brendan Cicero <brendan.cicero3@gmail.com>
2022-12-12 05:42:48 +0000

    Update Liking_Resta.spec.js

--- e2e/Liking_Resta.spec.js
+++ e2e/Liking_Resta.spec.js
@@ -31,3 +31,37 @@ Scenario("liking one resta", async ({ I }) => {
   const likedRestaTitle = await I.grabTextFrom(".resta__title");
   assert.strictEqual(firstRestaTitle, likedRestaTitle);
 });
+
+Scenario("unlike resta", async ({ I }) => {
+  I.see("Silahkan tambah resta favoritemu dulu di halaman utama", ".resta");
+
+  I.amOnPage("/");
+
+  I.waitForElement(".resta__list a", 10);
+  const firstResta = locate(".resta__list a").first();
+  const firstRestaTitle = await I.grabTextFrom(firstResta);
+  I.click(firstResta);
+
+  I.seeElement("#likeButton");
+  I.click("#likeButton");
+
+  I.amOnPage("/#/favorite");
+  I.seeElement(".resta__list");
+
+  const likedRestaTitle = await I.grabTextFrom(".resta__title");
+  assert.strictEqual(firstRestaTitle, likedRestaTitle);
+
+  I.click(likedRestaTitle);
+
+  I.seeElement("#likeButton");
+  I.click("#likeButton");
+
+  I.amOnPage("/#/favorite");
+  I.seeElement(".resta");
+  const unlikedResta = await I.grabTextFrom(".resta");
+
+  assert.strictEqual(
+    unlikedResta,
+    "Silahkan tambah resta favoritemu dulu di halaman utama"
+  );
+});
